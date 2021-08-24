2fb5964ac10b46c0f34a53a6452d2f07b07f8ca6
Athul Coder <88123015+SWEathul@users.noreply.github.com>
2021-08-24 10:19:45 +0530

    Create Name_age_code.cpp

---
+++ Name_age_code.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+   string name;
+   int age;
+ 
+   cout<<"What's your Name?\n";
+   cin>>name;
+   cout<<"Enter Your Age:\n";
+   cin>>age;
+    
+    
+    
+   cout<<name<<" is " <<age;
+    return 0;
+    
+}
